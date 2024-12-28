6d523a7a5436079b1ea8293e6bbbd3531df2bb8f
Drdla <drdla@gmail.com>
2024-12-28 07:10:02 +0100

    Register movie service in program.cs

--- MovieR.API/Program.cs
+++ MovieR.API/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IScreeningRoomService, ScreeningRoomService>();
 builder.Services.AddScoped<IScreeningService, ScreeningService>();
+builder.Services.AddScoped<IMovieService, MovieService>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
